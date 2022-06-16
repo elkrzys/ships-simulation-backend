202cc2b6634cd79f8048290642d3dae03676e8e1
Krzysiek <krzysztof.lipiec@outlook.com>
2022-06-16 02:58:03 +0200

    Implement at least one field space between ships

--- ShipsSimulationBackend/ShipsSimulationBackend/Models/Position.cs
+++ ShipsSimulationBackend/ShipsSimulationBackend/Models/Position.cs
@@ -5,4 +5,10 @@ public record Position(int Row, int Column)
     public int Row { get; set; } = Row;
     
     public int Column { get; set; } = Column;
+
+    public Position(Position position)
+    {
+        Row = position.Row;
+        Column = position.Column;
+    }
 }
